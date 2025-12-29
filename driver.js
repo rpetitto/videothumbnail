7362ce6b4743626ee3962bf5439f25f19a14e9ab
rpetitto <r.petitto@gmail.com>
2025-12-29 16:25:16 -0500

    Create driver.js
    Signed-off-by: rpetitto <r.petitto@gmail.com>

---
+++ driver.js
@@ -0,0 +1,27 @@
+window.addEventListener("message", async function (event) {
+  const { origin, data: { key, params } } = event;
+
+  let result;
+  let error;
+  try {
+    // We await the function in case it is async
+    result = await window.function(...params);
+  } catch (e) {
+    result = undefined;
+    try {
+      error = e.toString();
+    } catch (e) {
+      error = "Exception can't be stringified.";
+    }
+  }
+
+  const response = { key };
+  if (result !== undefined) {
+    response.result = { value: result };
+  }
+  if (error !== undefined) {
+    response.error = error;
+  }
+
+  event.source.postMessage(response, "*");
+});
